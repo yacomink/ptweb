7f9f0638ecbb5257351db43bd3d09fd837ef1266
ʞuıɯ-oɔɐʎ ʎpuɐ <andy@buzzfeed.com>
2013-10-01 15:12:06 -0400

    help

--- web.rb
+++ web.rb
@@ -8,8 +8,12 @@ require 'simple_client.rb'
 
 post '/:project_id' do
 
-    @simple = SimpleClient.new( ENV['PIVOTAL_API_KEY'] )
-	story = @simple.story( {:project_id => params[:project_id], :story_id => params[:text] }  )
+	if params[:text]
+	    @simple = SimpleClient.new( ENV['PIVOTAL_API_KEY'] )
+		story = @simple.story( {:project_id => params[:project_id], :story_id => params[:text] }  )
 
-	"#{story['name']} -\n#{story['description']}\n\n#{story['url']}"
+		"#{story['name']} -\n#{story['description']}\n\n#{story['url']}"
+	else
+		"Try /pt [story_id]"
+	end
 end
